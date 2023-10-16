42dfe10c59e32367a2e29f88f11294ce97cb0e03
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-10-16 11:56:55 -0300

    chore: setup tests for application to use jest

---
+++ src/entity/customer.spec.ts
@@ -0,0 +1,14 @@
+/**
+ * file: src/entity/customer.spec.ts
+ * description: file responsible for test the Customer class
+ * data: 10/16/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+describe("Customer unit tests", () => {
+
+  it("should return '1' as result", () => {
+    const result = 1;
+    expect(result).toBe(1);
+  });
+});
