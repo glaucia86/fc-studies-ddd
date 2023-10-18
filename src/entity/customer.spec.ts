1ea86e5ac3a1b9fd2a24aa51280afa056f05a119
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-10-18 20:46:46 -0300

    chore: test commit

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -5,10 +5,19 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
+import Customer from "./customer";
+
 describe("Customer unit tests", () => {
 
-  it("should return '1' as result", () => {
-    const result = 1;
-    expect(result).toBe(1);
+  it("should throw an error when 'id' is empty", () => {
+    expect(() => {
+      new Customer("", "Glaucia Lemos");
+    }).toThrowError("Id is required");
+  });
+
+  it("should throw an error when 'name' is empty", () => {
+    expect(() => {
+      new Customer("1234", "");
+    }).toThrowError("Name is required");
   });
 });
