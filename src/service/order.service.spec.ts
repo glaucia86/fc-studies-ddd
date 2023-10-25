8fb87e3d0ad27932c639801febb1492153f6da5e
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-10-25 19:44:08 -0300

    test: create test for 'ProductService'

---
+++ src/service/order.service.spec.ts
@@ -0,0 +1,22 @@
+/**
+ * file: src/service/order.service.spec.ts
+ * description: file responsible for test the 'Order Service' class
+ * data: 10/25/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import Product from "../entity/product";
+
+describe("Product service unit tests", () => {
+
+  it("should change the prices of all products", () => {
+    const product_01 = new Product("productId_01", "Mouse", 10);
+    const product_02 = new Product("productId_02", "Keyboard", 20);
+    const products = [product_01, product_02];
+
+    ProductService.increaseProductPrice(products, 100);
+
+    expect(product_01.price).toBe(20);
+    expect(product_02.price).toBe(40);
+  });
+})
