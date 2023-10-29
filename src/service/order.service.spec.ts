bfaccf6677fccb953fa42c35100679cdaec69088
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-10-29 12:22:45 -0300

    test: define 'Reward' policies

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -5,11 +5,23 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
 
 describe("Order service unit tests", () => {
+
+  it("should place an order", () => {
+
+    const customer_01 = new Customer("customer_1", "customer-name-01");
+    const item_01 = new OrderItem("id_1", "item-name-01", 10, 1, "prod-id-01");
+
+    const totalOrder = OrderService.placeOrder(customer_01, [item_01]);
+
+    expect(customer_01.rewardPoints).toBe(5);
+    expect(totalOrder).toBe(10);
+  })
 
   it("should get total of all orders ", () => {
 
