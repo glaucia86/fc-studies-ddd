773683080576fe938bf4960cce09ab7276166b40
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-06 20:04:29 -0300

    chore: register the first event

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -11,20 +11,30 @@ import EventInterface from "./event.interface";
 
 export default class EventDispatcher implements EventDispatcherInterface {
 
+  private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+  get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+    return this.eventHandlers;
+  }
+
   notify(event: EventInterface): void {
-    throw new Error("Method not implemented.");
+
   }
 
-  register(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
-    throw new Error("Method not implemented.");
+  register(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+
+    this.eventHandlers[eventName].push(eventHandler);
   }
 
-  unregister(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
-    throw new Error("Method not implemented.");
+  unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+
   }
-  
+
   unregisterAll(): void {
-    throw new Error("Method not implemented.");
+
   }
 
 }
